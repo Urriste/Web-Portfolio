d8f4ee242732b608d9891fc613fd9503678c34b9
Lucas Urriste <56521332+Urriste@users.noreply.github.com>
2020-12-17 13:02:39 -0300

    Add files via upload

---
+++ Portfolio/js/app.js
@@ -0,0 +1,10 @@
+const typed = new Typed('.typed', {
+	strings: ['<i class="yellow-span">Lucas Urriste</i>', '<i class="salmon-span">Frontend</i>', '<i class="yellow-span">Desarrollador</i>', '<i class="salmon-span">Autodidacta</i>'],
+	typeSpeed: 40,
+	startDelay: 300,
+	backSpeed: 45,
+	backDelay:1500,
+	loop:true,
+	loopCount: false
+
+});
